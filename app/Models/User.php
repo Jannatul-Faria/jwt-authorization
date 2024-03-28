69d8d5f4a062cf9717acd5b6bfb44eab50adae92
Jannatul-Faria <kairasana8@gmail.com>
2024-03-29 01:25:41 +0600

    auth

---
+++ app/Models/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class User extends Model
+{
+   
+
+    protected $fillable = [
+        'firstName',
+        'lastName',
+        'email',
+        'password',
+        'mobile',
+        'otp',
+    ];
+
+    protected  $attributes  = [
+        'otp' => '0'
+    ];
+
+
+
+  
+}
